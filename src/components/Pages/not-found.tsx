b13e2ede1eed9ec60158734e8218f03e3f8b5f44
areeba-awan <awanareeba40@gmail.com>
2024-12-13 22:34:02 +0500

    updated hackathon done

--- src/components/Pages/not-found.tsx
+++ src/components/Pages/not-found.tsx
@@ -9,7 +9,7 @@ const Custom404 = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6">
       <h1 className="text-8xl font-bold text-red-500 mb-4">404</h1>
       <h2 className="text-2xl text-gray-700 mb-8 text-center">Oops! The page you are looking for does not exist.</h2>
-         <Link href={"http://localhost:3000/"}>
+         <Link href={"./"}>
         <a className="px-6 py-3 bg-blue-600 text-white rounded-lg text-lg hover:bg-blue-700 transition-colors">Back to Homepage</a>
         </Link>
     </div>
