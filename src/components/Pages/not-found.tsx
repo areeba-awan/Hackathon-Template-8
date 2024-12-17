fcaa48fca55dc1bbdfb66074928376377106536a
areeba-awan <awanareeba40@gmail.com>
2024-12-17 22:15:14 +0500

    update hackathon

--- src/components/Pages/not-found.tsx
+++ src/components/Pages/not-found.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const Custom404 = () => {
   return (
-    <div className="flex flex-col items-center justify-center  overflow-x-hidden min-h-screen bg-gray-100 px-6">
+    <div className="flex flex-col items-center justify-center   min-h-screen bg-gray-100 px-6">
       <h1 className="text-8xl font-bold text-red-500 mb-4 ">404</h1>
       <h2 className="text-2xl text-gray-700 mb-8 text-center">Oops! The page you are looking for does not exist.</h2>
          <Link href={"./"}>
